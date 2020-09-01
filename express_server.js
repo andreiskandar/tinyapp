eff8e367386c04a8c484940a3708fe6b51f091da
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-01 16:06:45 -0700

    header login

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const alphanumeric = require('alphanumeric-id');
 
 // const ejs = require('ejs');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const PORT = 8080;
 
@@ -11,7 +14,6 @@ const generateRandomString = () => {
 	return alphanumeric(6);
 };
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -20,7 +22,7 @@ const urlDatabase = {
 };
 
 app.get('/', (req, res) => {
-	res.send('Hello!');
+	console.log(`Cookies: ${req.cookies}`);
 });
 
 app.get('/urls.json', (req, res) => {
@@ -28,7 +30,8 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-	let templateVars = { urls: urlDatabase };
+	console.log('req.cookies :', req.cookies);
+	let templateVars = { urls: urlDatabase, username: req.cookies.username };
 
 	//res.render('fileName in views folder', {object})
 	res.render('urls_index', templateVars); // second argument takes on object
@@ -43,6 +46,19 @@ app.get('/urls/new', (req, res) => {
 	res.render('urls_new');
 });
 
+app.post('/urls/login', (req, res) => {
+	res.cookie('username', req.body.username);
+	console.log(`res.cookies: ${res.cookie}`);
+
+	res.redirect('/urls');
+});
+
+// app.post('/login', (req, res) => {
+// 	let templateVars = { urls: urlDatabase /*username: req.cookies['username']*/ };
+
+// 	res.render('urls_index', templateVars);
+// });
+
 app.get('/urls/:shortURL', (req, res) => {
 	let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
 	res.render('urls_show', templateVars);
@@ -53,6 +69,7 @@ app.post('/urls', (req, res) => {
 	urlDatabase[shortURL] = req.body.longURL;
 	console.log(urlDatabase);
 	// res.send('Ok');
+	res.redirect('/urls');
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -74,7 +91,6 @@ app.post('/urls/:shortURL', (req, res) => {
 
 app.get('/u/:shortURL', (req, res) => {
 	const longURL = urlDatabase[req.params.shortURL];
-	console.log('longURL:', longURL);
 	res.redirect(longURL);
 });
 // app.get('/set', (req, res) => {
